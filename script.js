788acdfa5ed9a8054358411ba7049d056cea055d
tongg-c <tongg.chris@gmail.com>
2022-09-30 13:19:08 -0700

    refactored form validation logic and fixed "Passwords do no match" bug

--- script.js
+++ script.js
@@ -3,59 +3,39 @@ const confirmPassordInput = document.getElementById('confirm-password');
 const submitButton = document.querySelector('.submit-section label')
 const passwordErrorP = document.querySelector('.label-and-input p')
 
-let confirmPassword;
-let password;
+let confirmPassword = '';
+let password = '';
 
 passwordInput.addEventListener('keyup', e => {
-    if(e.target.value === ""){
-        return
-    }
     password = e.target.value;
     checkPassword();
 });
 
 confirmPassordInput.addEventListener('keyup', e => {
-    if(e.target.value === ""){
-        return
-    }
     confirmPassword = e.target.value;
     checkPassword();
 });
 
 function checkPassword(){
-    if(passwordInput.value === undefined){
-        return
-    }
-    if(confirmPassword.length < 6 || password.length < 6){
+    if(confirmPassword.length < 6 && password.length < 6){
         passwordErrorP.textContent = '* Password needs to be at least 6 characters';
-
-    } else {
-        passwordErrorP.textContent = '* Passwords do not match';
-    }
-
-    if(confirmPassword !== password || confirmPassword.length < 6 || password.length < 6){
+        passwordErrorP.removeAttribute('hidden');
         passwordInput.classList.add('invalid');
         confirmPassordInput.classList.add('invalid');
+    } else if(confirmPassword !== password){
+        passwordErrorP.textContent = '* Passwords do not match';
         passwordErrorP.removeAttribute('hidden');
+        passwordInput.classList.add('invalid');
+        confirmPassordInput.classList.add('invalid');
     } else {
         passwordInput.classList.remove('invalid');
         confirmPassordInput.classList.remove('invalid');
-        passwordErrorP.setAttribute('hidden',true);
+        passwordErrorP.setAttribute('hidden', true);
     }
 }
 
-
-
 submitButton.addEventListener('click', () =>{
-    if(confirmPassword.length < 6 || password.length < 6){
-        alert("Password has to be at least 6 characters");
-        return
-    }
-    if(confirmPassword !== password){
-        alert("Passwords do not match")
-        return
-    }
-    if(confirmPassword === password){
-        document.querySelector('form').submit();
-    }
+    if(confirmPassword.length < 6 || password.length < 6) alert("Password has to be at least 6 characters");
+    else if(confirmPassword !== password) alert("Passwords do not match");
+    else document.querySelector('form').submit();
 });
